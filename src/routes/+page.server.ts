428088388dd3b45ecfdd549570d92b6fed5a3a0e
TellMin <dhgirkcls@gmail.com>
2023-09-24 21:32:59 +0900

    Add form action to insert user

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { createD1Client } from '$lib/server/d1client';
 import { users } from '../schema';
 
@@ -7,3 +7,16 @@ export const load = (async ({ platform }) => {
 	const result = await d1Client.select().from(users).all();
 	return { users: result };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ platform, request }) => {
+		const data = await request.formData();
+		const name = data.get('name');
+		if (!name) {
+			return { success: false, error: 'Name is required' };
+		}
+		const d1Client = createD1Client(platform?.env?.DB);
+		await d1Client.insert(users).values({ name: name?.toString() }).execute();
+		return { success: true };
+	}
+} satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData, ActionData } from './$types';
 	export let data: PageData;
+	export let form: ActionData;
 </script>
 
-<h1>Users</h1>
+<h1>Regist User</h1>
+
+{#if form && !form.success}
+	<p>{form.error}</p>
+{/if}
+
+<form method="POST">
+	<label>Name<input name="name" type="text" /></label>
+	<button type="submit">Submit</button>
+</form>
 
 {#each data.users as user}
 	<p>{user.id}:{user.name}</p>
